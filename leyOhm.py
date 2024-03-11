2da3859b598506d518de7fd4cf83b3018a7f2e52
bporras <bporras2005@gmail.com>
2024-03-11 12:31:59 -0600

    Create leyOhm.py
    se agrega python Ohm

---
+++ leyOhm.py
@@ -0,0 +1,24 @@
+print ('-----------------------')
+print (' Programa de la Ley dee Ohm')
+print ('-----------------------')
+print (' devby: BPM')
+
+opcion = int(input('digite 1 para calcular el voltaje, 2 para calcular resistencia , 3 para calcular corrieente: '))
+
+if opcion == 1:
+    C = float(input('digite la corriente: '))
+    R = float(input('digite la resistencia:: '))
+    V= C * R
+if opcion == 2:
+    C = float(input('digite el corriente: '))
+    V= float(input('digite la volteje:: '))
+    R= V / C
+if opcion == 3:
+    V = float(input('digite el Voltaje: '))
+    R = float(input('digite la Corriente: '))
+    C =  V / R
+    
+print ('Resultados')
+print (' el voltaje es: ', V, 'voltios')
+print (' La  Corriente es: ',C, ' amperios')
+print (' La  Resistencia  es: ', R, ' ohmios')
